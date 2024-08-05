d832ac2676a73f4b5069eeea02ba9ba20fcbf013
Rutvik Patel <rutvik.p4t3l@gmail.com>
2024-08-05 01:48:47 +0000

    add constants for paths

---
+++ simulation/src/utils/constants.py
@@ -0,0 +1,21 @@
+import os
+from types import SimpleNamespace
+
+
+SIM_ROOT_PATH = os.path.join(__file__, '../..')
+REPO_ROOT = os.path.join(SIM_ROOT_PATH, '..')
+
+
+RES = SimpleNamespace(
+    UR5_MODEL_DIR=os.path.join(SIM_ROOT_PATH, 'model/ur5e.xml')
+)
+
+
+if __name__ == '__main__':
+    current_module = globals()
+    constants = {name: value for name, value in current_module.items() 
+                 if not name.startswith('__') and not callable(value)}
+    
+    print('Constants:')
+    for name, value in constants.items():
+        print(f'{name} = {value}')
