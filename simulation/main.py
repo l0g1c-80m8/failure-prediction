491f99e36e57db4cf7abefe8fe8efa43c71e8e9a
Rutvik Patel <rutvik.p4t3l@gmail.com>
2024-08-05 04:27:19 +0000

    fix the simulation to launch on a viewer

--- simulation/main.py
+++ simulation/main.py
@@ -13,11 +13,7 @@ def main() -> None:
     initial_qpos: List[float] = [0, -1.57, 1.57, -1.57, -1.57, 0]
     target_qpos: List[float] = [1.0, -1.0, 1.0, -1.0, -1.0, 1.0]
 
-    simulator.create_viewer()
-
     simulator.run_trajectory(initial_qpos, target_qpos, duration=5.0)
-
-    # simulator.keep_viewer_open()
 
 if __name__ == '__main__':
     # 🚀🚀🚀
--- simulation/src/simulation.py
+++ simulation/src/simulation.py
@@ -1,5 +1,5 @@
 import mujoco
-from mujoco_viewer import MujocoViewer
+import mujoco.viewer
 import numpy as np
 import time
 from typing import List, Optional
@@ -11,16 +11,11 @@ class MjSimulation:
     def __init__(self, model_path: str) -> None:
         self._model: mujoco.MjModel = mujoco.MjModel.from_xml_path(model_path)
         self._data: mujoco.MjData = mujoco.MjData(self._model)
-        self._viewer: Optional[MujocoViewer] = None
     
     def set_joint_positions(self, positions: List[float]) -> None:
         if len(positions) != self._model.nq:
             raise ValueError(f'Expected {self._model.nq} joint positions, got {len(positions)}')
         self._data.qpos[:] = positions
-    
-    def create_viewer(self) -> None:
-        if self._viewer is None:
-            self._viewer = MujocoViewer(self._model, self._data)
     
     def run_trajectory(
         self, 
@@ -30,29 +25,25 @@ class MjSimulation:
     ) -> None:        
         self.set_joint_positions(initial_qpos)
         
-        dt: float = self._model.opt.timestep
-        steps: int = int(duration / dt)
+        with mujoco.viewer.launch_passive(self._model, self._data) as viewer:
+            dt: float = self._model.opt.timestep
+            steps: int = int(duration / dt)
+            start = time.time()
+            idx = 0
 
-        for idx in range(steps):
-            LOGGER.info(f'on step {1 + idx} of {steps} steps')
-            t: float = idx / steps
-            current_qpos: np.ndarray = np.array(initial_qpos) + \
-                t * (np.array(target_qpos) - np.array(initial_qpos))
-            
-            self.set_joint_positions(current_qpos.tolist())
-            mujoco.mj_step(self._model, self._data)
-            
-            if self._viewer:
-                self._viewer.render()
-            
-            time.sleep(dt)
-
-    def keep_viewer_open(self) -> None:
-        if self._viewer:
-            while self._viewer.is_alive:
-                self._viewer.render()
-
-    def close_viewer(self) -> None:
-        if self._viewer:
-            self._viewer.close()
-            self._viewer = None
+            while viewer.is_running() and (idx := idx + 1) < steps:
+                step_start = time.time()
+                
+                LOGGER.info(f'on step {1 + idx} of {steps} steps')
+                t: float = idx / steps
+                current_qpos: np.ndarray = np.array(initial_qpos) + \
+                    t * (np.array(target_qpos) - np.array(initial_qpos))
+                
+                self.set_joint_positions(current_qpos.tolist())
+                mujoco.mj_step(self._model, self._data)
+                
+                viewer.sync()
+                
+                time_until_next_step = dt - (time.time() - step_start)
+                if time_until_next_step > 0:
+                    time.sleep(time_until_next_step)
