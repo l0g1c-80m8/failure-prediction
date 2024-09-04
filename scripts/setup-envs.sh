6fd6218ef1c05f20ad9884aa790a2244ccac5175
Rutvik Patel <rutvik.p4t3l@gmail.com>
2024-09-03 23:41:44 -0700

    use proper names for different venvs

--- scripts/setup-envs.sh
+++ scripts/setup-envs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # create the first virtual environment for the simulation package
-virtualenv /workspace/code/simulation/venv
-/workspace/code/simulation/venv/bin/pip install -r /workspace/code/simulation/requirements.txt
+virtualenv /workspace/code/simulation/sim_env
+/workspace/code/simulation/sim_env/bin/pip install -r /workspace/code/simulation/requirements.txt
 
 # create the second virtual environment for the octo package
-virtualenv /workspace/code/octo/venv
-/workspace/code/octo/venv/bin/pip install -r /workspace/code/octo/requirements.txt
+virtualenv /workspace/code/octo/octo_env
+/workspace/code/octo/octo_env/bin/pip install -r /workspace/code/octo/requirements.txt
 
 echo "[INFO]: Virtual environments are set up in /workspace/code/simulation/venv and /workspace/code/octo/venv!"
