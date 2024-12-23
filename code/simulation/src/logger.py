d2bf04db37171359945d1da61879ae995d664d5b
Rutvik Patel <rutvik.p4t3l@gmail.com>
2024-12-23 00:02:05 +0000

    reset everything

--- code/simulation/src/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-# local imports
-from src.constants import LOGGER_OPTIONS
-
-
-LOGGER = logging.getLogger(LOGGER_OPTIONS.NAME)
-logging.basicConfig(filename=LOGGER_OPTIONS.FILE, level=LOGGER_OPTIONS.LEVEL)
